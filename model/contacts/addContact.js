066e8e3e40b0060acd339f3e5a6a1d7517946fa9
Larysa1387 <kaliuzhna.off@gmail.com>
2021-09-27 02:05:00 +0300

    add joi validation, add hhtp-errors

---
+++ model/contacts/addContact.js
@@ -0,0 +1,16 @@
+const fs = require('fs/promises')
+const path = require('path')
+const { v4 } = require('uuid')
+const listContacts = require('./listContacts')
+
+const contactsPath = path.join(__dirname, '..', 'contacts.json')
+
+const addContact = async data => {
+  const allContacts = await listContacts()
+  const newContact = { id: v4(), ...data }
+  const updateContacts = [...allContacts, newContact]
+  await fs.writeFile(contactsPath, JSON.stringify(updateContacts))
+  return newContact
+}
+
+module.exports = addContact
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const { NotFound, BadRequest } = require('http-errors')
+const Joi = require('joi')
 const router = express.Router()
 const {
   listContacts,
-  // getContactById,
-  // addContact,
+  getContactById,
+  addContact,
   // removeContact,
 } = require('../../model/contacts/index')
+
+const joiSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net'] },
+    })
+    .required(),
+  phone: Joi.number().required(),
+})
 
 router.get('/', async (req, res, next) => {
   try {
@@ -17,11 +30,40 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+    const contact = await getContactById(contactId)
+    if (!contact) {
+      throw new NotFound(`Contact with id-${contactId} not found`)
+      // next(createError(404, `Contact with id-${contactId} not found`))
+      // res.status(404).json({
+      //   status: 'error',
+      //   code: 404,
+      //   message: `Contact with id-${contactId} not found`,
+      // })
+      // return
+    }
+    res.json(contact)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = joiSchema.validate(req.body)
+    if (error) {
+      throw new BadRequest(error.message)
+    }
+    const result = await addContact(req.body)
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: { result },
+    })
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {

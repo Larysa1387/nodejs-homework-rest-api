abc5b43a233030d052e6a6ae9c02913666173b99
Larysa1387 <kaliuzhna.off@gmail.com>
2021-10-14 01:47:32 +0300

    finished login

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,23 +1,27 @@
-const { NotFound } = require('http-errors')
+const { NotFound } = require('http-errors');
 
-const { Contact } = require('../models')
+const { Contact } = require('../models');
 
 const getAll = async (req, res, next) => {
-  const contacts = await Contact.find({}) // can add after {}, what we want to get: {}, "_id name email phone favorite"
+  const { _id } = req.user;
+  const contacts = await Contact.find({ owner: _id }).populate(
+    'owner',
+    'email'
+  ); // can add after {}, what we want to get: {}, "_id name email phone favorite"
   res.json({
     status: 'success',
     code: 200,
     data: {
       contacts,
     },
-  })
-}
+  });
+};
 
 const getById = async (req, res, next) => {
-  const { contactId } = req.params
-  const contact = await Contact.findById(contactId)
+  const { contactId } = req.params;
+  const contact = await Contact.findById(contactId);
   if (!contact) {
-    throw new NotFound(`Contact with id-${contactId} not found`)
+    throw new NotFound(`Contact with id-${contactId} not found`);
   }
   res.json({
     status: 'success',
@@ -25,65 +29,67 @@ const getById = async (req, res, next) => {
     data: {
       contact,
     },
-  })
-}
+  });
+};
 
 const add = async (req, res, next) => {
-  const result = await Contact.create(req.body)
+  const newContact = { ...req.body, owner: req.user._id };
+  // const result = await Contact.create(req.body);
+  const result = await Contact.create(newContact);
   res.status(201).json({
     status: 'success',
     code: 201,
     data: { result },
-  })
-}
+  });
+};
 
 const delById = async (req, res, next) => {
-  const { contactId } = req.params
-  const result = await Contact.findByIdAndDelete(contactId)
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndDelete(contactId);
   if (!result) {
-    throw new NotFound(`Contact with id-${contactId} not found`)
+    throw new NotFound(`Contact with id-${contactId} not found`);
   }
   res.json({
     status: 'success',
     code: 200,
     message: 'Contact deleted',
-  })
-}
+  });
+};
 
 const updateById = async (req, res, next) => {
-  const { contactId } = req.params
+  const { contactId } = req.params;
   const result = await Contact.findByIdAndUpdate(contactId, req.body, {
     new: true,
-  })
+  });
   if (!result) {
-    throw new NotFound(`Contact with id-${contactId} not found`)
+    throw new NotFound(`Contact with id-${contactId} not found`);
   }
   res.json({
     status: 'success',
     code: 200,
     data: { result },
-  })
-}
+  });
+};
 
 // Update status favorite in the contact
 const updateStatus = async (req, res, next) => {
-  const { contactId } = req.params
-  const { favorite } = req.body
+  const { contactId } = req.params;
+  const { favorite } = req.body;
   const result = await Contact.findByIdAndUpdate(
     contactId,
     { favorite },
     { new: true }
-  )
+  );
   if (!result) {
-    throw new NotFound(`Contact with id-${contactId} not found`)
+    throw new NotFound(`Contact with id-${contactId} not found`);
   }
   res.json({
     status: 'success',
     code: 200,
     message: 'Status of favorite updated',
     data: { result },
-  })
-}
+  });
+};
 
 module.exports = {
   getAll,
@@ -92,4 +98,4 @@ module.exports = {
   delById,
   updateById,
   updateStatus,
-}
+};
--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,3 +1,36 @@
-const login = async (req, res) => {}
+const { Unauthorized } = require('http-errors');
+const bCrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-module.exports = login
+const { User } = require('../../models/userModel');
+const { SECRET_KEY } = process.env;
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  // if (!user) {
+  //   throw new Unauthorized(`Email ${email} not found`);
+  // }
+  const isCorrectPassword = bCrypt.compareSync(password, user.password);
+  // if (!isCorrectPassword) {
+  //   throw new Unauthorized('Password incorrect');
+  // }
+  if (!user || !isCorrectPassword) {
+    throw new Unauthorized('Email or password is incorrect');
+  }
+
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY);
+
+  res.json({
+    status: 'success',
+    code: 200,
+    data: {
+      token,
+    },
+  });
+};
+
+module.exports = login;

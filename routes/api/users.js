aa89fb40f5f9ae75b76c0e3130b9d53306adf38c
Larysa1387 <kaliuzhna.off@gmail.com>
2021-10-14 01:44:56 +0300

    add routes for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,22 +1,21 @@
-const express = require('express')
+const express = require('express');
 
 const {
   controllerWrapper,
   validation,
   // favoriteValidation,
-} = require('../../middlewares')
+} = require('../../middlewares');
 
-const { joiSchema } = require('../../models/userModel')
+const { joiSchema } = require('../../models/userModel');
 
-const { users: ctrl } = require('../../controllers')
-// const { signup, login, logout } = require('../../controllers')
+const { users: ctrl } = require('../../controllers');
 
-const router = express.Router()
+const router = express.Router();
 
 // post on /api/users/signup
-router.post('/signup', validation(joiSchema), controllerWrapper(ctrl.signup))
-router.post('/login', controllerWrapper(ctrl.login))
-router.get('/logout', controllerWrapper(ctrl.logout))
+router.post('/signup', validation(joiSchema), controllerWrapper(ctrl.signup));
+router.post('/login', validation(joiSchema), controllerWrapper(ctrl.login));
+router.get('/logout', controllerWrapper(ctrl.logout));
 // router.post('/logout', controllerWrapper(ctrl.logout))
 
-module.exports = router
+module.exports = router;

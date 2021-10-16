95dc4cd0ab77dcb0339c5f8de3449354380d813a
Larysa1387 <kaliuzhna.off@gmail.com>
2021-10-16 20:12:58 +0300

    finished logout

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const {
   controllerWrapper,
   validation,
+  authenticate,
   // favoriteValidation,
 } = require('../../middlewares');
 
@@ -15,7 +16,7 @@ const router = express.Router();
 // post on /api/users/signup
 router.post('/signup', validation(joiSchema), controllerWrapper(ctrl.signup));
 router.post('/login', validation(joiSchema), controllerWrapper(ctrl.login));
-router.get('/logout', controllerWrapper(ctrl.logout));
+router.get('/logout', authenticate, controllerWrapper(ctrl.logout));
 // router.post('/logout', controllerWrapper(ctrl.logout))
 
 module.exports = router;
